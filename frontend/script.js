87761e9e05ac7d45ff03de8fce9a53b0a4f84d08
galvaolanches <tutorialsparapcfraco@gmail.com>
2026-02-04 19:06:42 -0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,11 @@
 function fazerPedido(produto) {
-  fetch("https://backend-cardapio.onrender.com/pedido", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ produto })
-  })
+  fetch("https://galvaolanches.onrender.com/pedido", {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ produto })
+})
   .then(res => res.json())
   .then(data => alert(data.status))
   .catch(err => console.error(err));
 }
+
